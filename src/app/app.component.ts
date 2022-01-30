16bc79bc707170163db7140b6f2a98cef9ab8cc9
luc-dev10 <luc.dev1008@gmail.com>
2022-01-30 09:56:23 -0500

    specify props

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'card-app';
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/tree.jpeg',
+      content: 'I saw this neat tree. It grows pineapples and bananas.',
+    },
+    {
+      title: 'Snowy Mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      content:
+        'Here is a picture of a snowy mountain. Enjoy the view from heaven.',
+    },
+    {
+      title: 'Biking through the snow',
+      imageUrl: 'assets/biking.jpeg',
+      content: 'Biking is great. A snowy day is the best day for biking.',
+    },
+  ];
 }
